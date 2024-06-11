dd97243ed81c9bf535fb896978efcdf7f45e8df0
ColoradaGreis <graciana.baratti@gmail.com>
2024-06-10 22:07:01 -0300

    chore: Update app.module.ts with Supabase and Mongoose configurations

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,25 +3,24 @@ import { SupabaseModule } from './supabase/supabase.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import { UsersModule } from './users/users.module';
 import { ConfigModule, ConfigService } from '@nestjs/config';
-import { UsersModule } from './users/users.module';
-
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true, // Hace que ConfigModule esté disponible globalmente en toda la aplicación.
       envFilePath: '.env', // Especifica el archivo de variables de entorno.
-      expandVariables: true // Permite la expansión de variables dentro del archivo .env
+      expandVariables: true, // Permite la expansión de variables dentro del archivo .env
     }),
     MongooseModule.forRootAsync({
       imports: [ConfigModule],
       useFactory: async (configService: ConfigService) => ({
-        uri: configService.get<string>('DATABASE_URI') // Utiliza una variable de entorno para la URI
+        uri: configService.get<string>('DATABASE_URI'), // Utiliza una variable de entorno para la URI
       }),
-      inject: [ConfigService]
+      inject: [ConfigService],
     }),
     SupabaseModule,
-    UsersModule],
+    UsersModule,
+  ],
   controllers: [],
   providers: [],
 })
