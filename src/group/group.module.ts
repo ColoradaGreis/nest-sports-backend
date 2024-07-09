815a16d29cbcc80d0084bcb8aa43d1fcd2285563
Graciana <graciana.baratti@gmail.com>
2024-07-09 20:13:11 -0300

    chore: Update @nestjs/mongoose dependency to version 10.0.10

--- src/group/group.module.ts
+++ src/group/group.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { GroupService } from './group.service';
 import { GroupController } from './group.controller';
-
+import { MongooseModule } from '@nestjs/mongoose';
+import { Group, GroupSchema } from 'src/schemas/group.schema';
 @Module({
+  imports: [
+    MongooseModule.forFeature([{ name: Group.name, schema: GroupSchema }]),
+  ],
+  controllers: [GroupController],
   providers: [GroupService],
-  controllers: [GroupController]
 })
 export class GroupModule {}
