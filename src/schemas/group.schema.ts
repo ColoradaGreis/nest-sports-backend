70457f1f03fb098ec91e83cfaaba866415d71029
ColoradaGreis <graciana.baratti@gmail.com>
2024-07-09 21:01:49 -0300

    refactor: Update Group schema to use User and Match schemas for owner, members, and matches

--- src/schemas/group.schema.ts
+++ src/schemas/group.schema.ts
@@ -1,19 +1,21 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import { Document, Types } from 'mongoose';
+import { User } from './user.schema';
+import { Match } from './match.schema';
 
 @Schema()
 export class Group extends Document {
   @Prop({ required: true })
   name: string;
 
-  @Prop({ type: Types.ObjectId, ref: 'User' })
-  owner: Types.ObjectId;
+  @Prop({ type: Types.ObjectId, ref: 'User', required: true })
+  owner: User;
 
-  @Prop([{ type: Types.ObjectId, ref: 'User' }])
-  members: Types.ObjectId[];
+  @Prop({ type: [{ type: Types.ObjectId, ref: 'User' }], default: [] })
+  members: User[];
 
-  @Prop([{ type: Types.ObjectId, ref: 'Match' }])
-  matches: Types.ObjectId[];
+  @Prop({ type: [{ type: Types.ObjectId, ref: 'Match' }], default: [] })
+  matches: Match[];
 }
 
 export const GroupSchema = SchemaFactory.createForClass(Group);
