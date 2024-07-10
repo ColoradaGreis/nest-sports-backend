e6ff893b91faaeea790bac872eb9cef6ea412065
ColoradaGreis <graciana.baratti@gmail.com>
2024-07-10 19:03:35 -0300

    refactor: Add MongooseModule for Match in MatchModule

--- src/match/match.module.ts
+++ src/match/match.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { MatchService } from './match.service';
 import { MatchController } from './match.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Match, MatchSchema } from 'src/schemas/match.schema';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([{ name: Match.name, schema: MatchSchema }]),
+  ],
   providers: [MatchService],
-  controllers: [MatchController]
+  controllers: [MatchController],
 })
 export class MatchModule {}
